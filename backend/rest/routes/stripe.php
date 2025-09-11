8689acb0983c8c7a6a14475abfe3de875c6fbb68
fadara82 <fadi.egho@stu.ibu.edu.ba>
2025-09-11 15:46:47 +0200

    Fin

--- backend/rest/routes/stripe.php
+++ backend/rest/routes/stripe.php
@@ -4,16 +4,15 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 use \Stripe\Stripe;
 use \Stripe\PaymentIntent;
 
-// Učitaj stripe.env
-$env = parse_ini_file(__DIR__ . '/stripe.env');
-$stripeSecretKey = $env['STRIPE_SECRET_KEY'] ?? null;
+// Uzmi Stripe key direktno iz Render environment varijable
+$stripeSecretKey = getenv('STRIPE_SECRET_KEY');
 
 Flight::route('POST /stripe/create-payment-intent', function() use ($stripeSecretKey) {
     $input = json_decode(file_get_contents('php://input'), true);
 
     $amount = $input['amount']; // amount u centima (npr. 1000 = 10.00 KM)
 
-    Stripe::setApiKey($stripeSecretKey); // sad ide iz env fajla
+    Stripe::setApiKey($stripeSecretKey);
 
     try {
         $paymentIntent = PaymentIntent::create([
