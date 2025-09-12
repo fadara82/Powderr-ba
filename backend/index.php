bf87c01236edb808a1db8196fdf6d4f80565d129
fadara82 <fadi.egho@stu.ibu.edu.ba>
2025-09-12 03:15:20 +0200

    Final

--- backend/index.php
+++ backend/index.php
@@ -8,6 +8,15 @@ require "rest/routes/users_routes.php";
 require "rest/routes/auth_routes.php";
 require 'rest/routes/stripe.php';
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 
 Flight::route('/', function () {
   echo 'hello world!';
