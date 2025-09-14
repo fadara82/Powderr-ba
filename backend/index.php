458e08056141b36c0434d5b25284ea1024632ae5
fadara82 <fadi.egho@stu.ibu.edu.ba>
2025-09-14 09:52:08 +0200

    Fin

--- backend/index.php
+++ backend/index.php
@@ -6,35 +6,42 @@ require "rest/routes/orders_routes.php";
 require "rest/routes/products_routes.php";
 require "rest/routes/users_routes.php";
 require "rest/routes/auth_routes.php";
-require 'rest/routes/stripe.php';
+require "rest/routes/stripe.php";
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
-Flight::route('OPTIONS /*', function() {
+function corsHeaders() {
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
+}
+
+Flight::before('start', function (&$params, &$output) {
+    corsHeaders();
+});
+
+Flight::route('OPTIONS /*', function() {
+    corsHeaders();
     http_response_code(200);
     exit();
 });
 
 Flight::route('/', function () {
-  echo 'hello world!';
+    echo 'hello world!';
 });
 
 Flight::route('/web', function () {
-  echo 'hello world sa Malte!';
+    echo 'hello world sa Malte!';
 });
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-Flight::map('error', function(Throwable $ex) {
-    Flight::halt(500, $ex->getMessage());
+Flight::map('error', function (Throwable $ex) {
+    Flight::json([
+        "status" => "error",
+        "message" => $ex->getMessage()
+    ], 500);
 });
 
-define("JWT_SECRET", getenv("JWT_SECRET"));
+define("JWT_SECRET", getenv("JWT_SECRET") ?: "changeme-secret");
 
 Flight::start();
